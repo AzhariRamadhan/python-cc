2ce6d8ad2c2e91bc24bf5f9b9426c37ffd5a5597
Azhari Ramadhan <32870854+azhari33@users.noreply.github.com>
2019-08-14 21:38:22 +0700

    Update

--- bab-5.py
+++ bab-5.py
@@ -49,6 +49,40 @@ elif 'yellow' in color_alien:
     print("Player earned 10 point")
 else:
     print("Player earned 15 point")
+    
+#5-6
+age = 15
+
+if age < 2:
+    print('The person is a baby')
+elif age >= 2 and age < 4:
+    print('The person is a toddler')
+elif age >= 4 and age < 13:
+    print('The person is a kid')
+elif age >= 13 and age < 20:
+    print('The person is a teenager')
+elif age >= 20 and age < 65:
+    print('The person is an adult')
+else:
+    print('The person is an elder')
+
+#5-7 Favorite Fruits
+favorite_fruits = ['banana', 'nectarine', 'peach', 'pears', 'apples']
+
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'peach' in favorite_fruits:
+    print('You really like peaches!')
+
+if 'cantaloupe' in favorite_fruits:
+    print('You really like cantaloupe!')
+
+if 'watermelon' in favorite_fruits:
+    print('You really like watermelon!')
+
+if 'pears' in favorite_fruits:
+    print('You really like pears!')
 
 #5-8 Hello Admin
 print("\n5-8 Hello Admin")
@@ -62,3 +96,28 @@ for user_login in user_logins:
         print("Hello {}, would you like to see a status report").format(user_login)
     else:
         print("Hello {}, thank you for logging in again").format(user_login)
+        
+#5-10
+current_users = ['admin', 'winston', 'ashley', 'eric', 'martin', 'ben']
+new_users = ['Winston', 'ashley', 'dianne', 'xia', 'calvin']
+
+for new_user in new_users:
+    if new_user.lower() in current_users:
+        print('You will need to enter a new username')
+    else:
+        print('The username is available')
+        
+#5-11
+numbers = [v for v in range(1, 10)]
+ordinal_suffix = ''
+
+for number in numbers:
+    if number == 1:
+        ordinal_suffix = 'st'
+    elif number == 2:
+        ordinal_suffix = 'nd'
+    elif number == 3:
+        ordinal_suffix = 'rd'
+    else:
+        ordinal_suffix = 'th'
+    print(str(number) + ordinal_suffix)
